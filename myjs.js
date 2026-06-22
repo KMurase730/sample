24aa863e8bbed439bb75bb98d1f51083e2f1f91d
KMurase730 <zm25031@josai.ac.jp>
2026-06-22 12:02:08 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,11 +5,11 @@ alert(Math.sqrt(2)+Math.sqrt(3))
 alert(1/0)
 alert(0/0)
 
-var sum=2+3
-var sum='2'+'3'
-var sum=Number('2')+Number('3')
-var sum='2'+3
-var sum=Number('2')+3
+alert(2+3)
+alert('2'+'3')
+alert(Number('2')+Number('3'))
+alert('2'+3)
+alert(Number('2')+3)
 
 
 function calculateArea(width, height) {
