c3683dad85aa27de3f2c2a85d4043bc832031ff1
KMurase730 <zm25031@josai.ac.jp>
2026-06-22 11:59:04 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,8 +5,8 @@ alert(Math.sqrt(2)+Math.sqrt(3))
 alert(1/0)
 alert(0/0)
 
-2+3
-'2'+'3'
-Number('2')+Number('3')
-'2'+3
-Number('2')+3
+var sum=2+3
+var sum='2'+'3'
+var sum=Number('2')+Number('3')
+var sum='2'+3
+var sum=Number('2')+3
